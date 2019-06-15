3b600a98507e19328dd328037f68e3d28b0506eb
Renato Marques Teles <renatomarquesteles@gmail.com>
2019-06-15 15:31:28 -0300

    correcao no nome da funçao do multer no upload

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 
 module.exports = {
-  storage: new multer.DiskStorage({
+  storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'), // Define para onde os arquivos vão
     filename(req, file, callback) {
       // Define o nome do arquivo para utilizar o nome original do arquivo
